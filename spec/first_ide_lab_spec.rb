3f735afedb55aa6e93b25658272d053170366ed6
ashleygurule <ashleykgurule@gmail.com>
2018-02-08 02:13:11 +0000

    Done.

--- spec/first_ide_lab_spec.rb
+++ spec/first_ide_lab_spec.rb
@@ -3,4 +3,4 @@ RSpec.describe 'First Lab:' do
     file_edited = !File.read("./edit-me.txt").empty?
     expect(file_edited).to be_truthy, "Make sure you have edited the file edit-me.txt"
   end
-end
+end 
